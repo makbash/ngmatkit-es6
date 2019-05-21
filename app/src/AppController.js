4de479343a305e367813a4b6bfd61a1e4cf355f8
Mustafa Akbaş <makbash21@gmail.com>
2019-05-21 16:46:55 +0300

    update sidenav

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -7,22 +7,45 @@
 
 import Routes from 'src/config/routes';
 
-function AppController($mdSidenav) {
+function AppController($scope, $mdSidenav, $mdMedia, $log) {
     const self = this;
 
+    self.routes = Routes;
     self.lockedLeft = localStorage.getItem('lockedLeft') === 'false' ? false : true;
+
+    self.mdScreen = $mdMedia('min-width: 960px');
+    $scope.$watch(() => {
+        return $mdMedia('min-width: 960px');
+    }, (res) => {
+        self.mdScreen = res;
+    });
 
     // *********************************
     // Internal methods
     // *********************************
 
-    /**  Hide or Show the 'left' sideNav area */
-    self.toggleLeft = function () {
-        self.lockedLeft = !self.lockedLeft;
-        localStorage.setItem('lockedLeft', self.lockedLeft);
+    self.closeSideNav = () => {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav('left').close()
+                .then(() => {
+                    $log.debug("close LEFT is done");
+                });
+
     };
 
-    self.sidenav = Routes;
+    /**  Hide or Show the 'left' sideNav area */
+    self.toggleLeft = () => {
+        if (self.mdScreen) {
+            self.lockedLeft = !self.lockedLeft;
+            localStorage.setItem('lockedLeft', self.lockedLeft);
+        } else {
+            $mdSidenav('left')
+                    .toggle()
+                    .then(() => {
+                        $log.debug("toggle left is done");
+                    });
+        }
+    };
 }
 
-export default ['$mdSidenav', AppController];
+export default ['$scope', '$mdSidenav', '$mdMedia', '$log', AppController];
