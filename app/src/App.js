bb9ac7684ac2119592e27d71993149886f3cc885
Mustafa Akbaş <makbash21@gmail.com>
2019-07-12 17:09:31 +0300

    add login module

--- app/src/App.js
+++ app/src/App.js
@@ -10,50 +10,52 @@ import 'angular-material';
 import Routes from 'src/config/routes';
 import AppController from 'src/AppController';
 
+import Login from './modules/login/Module';
 import Home from './modules/home/Module';
 import Example from './modules/example/Module';
 import About from './modules/about/Module';
 import Users from './modules/users/Module';
 
 export default angular
-        .module('starter-app', ['ngMaterial', 'ui.router',
-            Home.name,
-            Example.name,
-            About.name,
-            Users.name
-        ])
-        .config(($mdThemingProvider, $mdIconProvider, $locationProvider, $stateProvider) => {
-//            console.log('About.name', About.name);
+    .module('starter-app', ['ngMaterial', 'ui.router',
+        Home.name,
+        Example.name,
+        About.name,
+        Users.name,
+        Login.name
+    ])
+    .config(($mdThemingProvider, $mdIconProvider, $locationProvider, $stateProvider) => {
+        //            console.log('About.name', About.name);
 
-            // Loop over the state definitions and register them
-            Routes.forEach(function (state) {
-                $stateProvider.state(state);
-            });
+        // Loop over the state definitions and register them
+        Routes.forEach(function (state) {
+            $stateProvider.state(state);
+        });
 
 
-            // Register the user `avatar` icons
-            $mdIconProvider
-                    .defaultIconSet("./assets/svg/avatars.svg", 128)
-                    .icon("menu", "./assets/svg/menu.svg", 24)
-                    .icon("share", "./assets/svg/share.svg", 24)
-                    .icon("google_plus", "./assets/svg/google_plus.svg", 24)
-                    .icon("hangouts", "./assets/svg/hangouts.svg", 24)
-                    .icon("twitter", "./assets/svg/twitter.svg", 24)
-                    .icon("phone", "./assets/svg/phone.svg", 24);
+        // Register the user `avatar` icons
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+            .icon("twitter", "./assets/svg/twitter.svg", 24)
+            .icon("phone", "./assets/svg/phone.svg", 24);
 
-            $mdThemingProvider.theme('default')
-                    .primaryPalette('blue')
-                    .accentPalette('red');
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
 
-//            $locationProvider.html5Mode(true);
-            $locationProvider.html5Mode({
-                enabled: false,
-                requireBase: true
-            });
+        //            $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode({
+            enabled: false,
+            requireBase: true
+        });
 
-        })
-        .run(['$log', ($log) => {
-                $log.info("app started");
-            }
-        ])
-        .controller('AppController', AppController);
+    })
+    .run(['$log', ($log) => {
+        $log.info("app started");
+    }
+    ])
+    .controller('AppController', AppController);
